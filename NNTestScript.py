3d3d695172bd66cc9e5d762b0a2ae71ff56c3777
ritchie <ritchie_w@outlook.com>
2016-09-21 13:42:31 +0800

    digit classification using BPNN.py

---
+++ NNTestScript.py
@@ -0,0 +1,16 @@
+import numpy as np
+from sklearn import datasets
+from BP import FNN
+
+iris = datasets.load_digits()
+idx = np.random.uniform(0, 1500, (1500, )).astype('int')
+idx = set(idx)
+ridx = set(range(1797))-idx
+idx, ridx = np.array(list(idx)), np.array(list(ridx))
+X, y, test, label = iris.data[idx,:], iris.target[idx], iris.data[ridx,:], iris.target[ridx]
+
+nn = FNN(layer_nodes=[64, 128, 100, 10], activation='sigmoid')
+nn.train(X, y, epoch=5000)
+y_hat = nn.predict(test)
+print zip(y_hat, label)
+print 1.*len([item for item in zip(y_hat, label) if item[0] == item[1]])/len(label)
