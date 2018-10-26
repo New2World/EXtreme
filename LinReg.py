e42aeebc7d7c2b0823b1b91485c52fafa1c153e3
Ritchie <ac.ritchiew@gmail.com>
2018-10-25 20:43:52 -0400

    adjust inheritance

--- LinReg.py
+++ LinReg.py
@@ -1,14 +1,17 @@
 import numpy as np
-from base import *
+from SupervisedBase import SupervisedBaseClass
 
-class LinReg(base):
+class LinReg(SupervisedBaseClass):
+
+    def __init__(self):
+        pass
 
     def train(self, X, y):
-        X, y = self.formatData(X, y)
+        X, y = self._format_batch(X, y)
         temp_mat = X.transpose().dot(X)
         inv_mat = np.linalg.pinv(temp_mat)
         self.__w = (inv_mat.dot(X.transpose().dot(y.transpose()))).transpose()
 
     def predict(self, X):
-        X = self.formatData(X)[0]
+        X = self._format_batch(X)
         return (self.__w.dot(X.transpose())).tolist()
