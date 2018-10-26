8524bffbf34893a237778c53d83a9d712ede216e
Ritchie <ac.ritchiew@gmail.com>
2018-10-25 20:46:08 -0400

    adjust name of some functions

--- Softmax.py
+++ Softmax.py
@@ -1,41 +1,41 @@
-#!/bin/env python
-
 import numpy as np
 from SupervisedBase import SupervisedBaseClass
 
 class Softmax(SupervisedBaseClass):
 
-    def __init__(self, alpha=0.001, max_iter=500):
+    def __init__(self, alpha=0.001):
         self.alpha = alpha
-        self.max_iter = max_iter
+        self.sample = 0
+        self.feature = 0
 
-    def __initArg(self, X, y):
-        self._initSize(X, y)
-        self.theta = np.mat(np.zeros((self.K, X.shape[1])))
+    def __init_arg(self, X, y):
+        self.sample, self.feature = X.shape
+        self.K = len(set(y))
+        self.theta = np.zeros((self.K, X.shape[1]))
 
-    def __calcEach(self, x):
+    def __calc_each(self, x):
         pred = np.exp(self.theta*x)
         normalizer = np.sum(pred)
         pred = pred/normalizer
         return pred
 
-    def train(self, X, y):
-        X = self.formatData(X)[0]
-        self.__initArg(X, y)
+    def train(self, X, y, max_iter=1000):
+        X = self._format_batch(X)
+        self.__init_arg(X, y)
         X = X.T
         loop = 0
         print 'training...'
-        while loop < self.max_iter:
+        while loop < max_iter:
             loop += 1
             for i in xrange(self.sample):
-                pred = self.__calcEach(X[:,i])
+                pred = self.__calc_each(X[:,i])
                 updateItem = self.alpha*(1-pred[y[i],0])*X[:,i].T
-                self.theta[y[i],:] = self.theta[y[i],:]+updateItem
+                self.theta[y[i],:] = self.theta[y[i],:]+updateItem.reshape((1,-1))
 
     def predict(self, X):
-        X = self.formatData(X)[0]
+        X = self._format_batch(X)
         X = X.T; result = []
         for i in xrange(X.shape[1]):
-            pred = self.__calcEach(X[:,i])
+            pred = self.__calc_each(X[:,i])
             result.append(np.argmax(pred))
         return result
